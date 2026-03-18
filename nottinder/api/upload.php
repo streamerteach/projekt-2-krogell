d7ec6ed9b1f3958b4e541f2ec62c30baeb4383e6
Henry <50325487+hkrogell@users.noreply.github.com>
2026-03-18 09:36:25 +0200

    Delete nottinder directory
    Major issues, correcting and pushing new version.

--- nottinder/api/upload.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once '../includes/handlers/upload_handler.php';
--- nottinder/templates/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-    <footer>
-        <p>NotTinder - Find your perfect mismatch!</p>
-    </footer>
-    <script src="/nottinder/assets/js/infinite-scroll.js"></script>
-</body>
-</html>
